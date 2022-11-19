29b88fe241a93b72de6149dafea2e0de92017e6d
adrianrevilla009 <adrianrg1996@gmail.com>
2022-11-19 17:34:35 +0100

    add write board into controller

--- src/main/java/org/example/modelViewPresenter/refactorToPassiveView/controller/Controller.java
+++ src/main/java/org/example/modelViewPresenter/refactorToPassiveView/controller/Controller.java
@@ -1,5 +1,6 @@
 package org.example.modelViewPresenter.refactorToPassiveView.controller;
 
+import org.example.modelViewPresenter.refactorToPassiveView.view.console.BoardView;
 import org.example.modelViewPresenter.refactorToPassiveView.view.ViewFactory;
 import org.example.modelViewPresenter.refactorToPassiveView.domain.Cell;
 import org.example.modelViewPresenter.refactorToPassiveView.domain.Game;
@@ -13,6 +14,16 @@ public abstract class Controller {
     Controller(Game game, ViewFactory viewFactory) {
         this.game = game;
         this.viewFactory = viewFactory;
+    }
+
+    void writeBoard() {
+        BoardView boardView = this.viewFactory.createBoardView();
+        for (int i = 0; i < Cell.ROWS; i++) {
+            for (int j = 0; j < Cell.COLUMNS; j++) {
+                boardView.set(this.game.getColor(new Cell(i, j)));
+            }
+        }
+        boardView.write();
     }
 
     public Color getColor(Cell cell) {
