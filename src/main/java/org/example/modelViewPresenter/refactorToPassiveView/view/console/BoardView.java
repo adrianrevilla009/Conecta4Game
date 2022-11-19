18111afebfbde58bfe324998af8a12af74e55761
adrianrevilla009 <adrianrg1996@gmail.com>
2022-11-19 18:04:46 +0100

    clean unused view methods

--- src/main/java/org/example/modelViewPresenter/refactorToPassiveView/view/console/BoardView.java
+++ src/main/java/org/example/modelViewPresenter/refactorToPassiveView/view/console/BoardView.java
@@ -10,18 +10,6 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class BoardView {
-    void write(Controller controller) {
-        new MessageView().writeln(Message.HORIZONTAL_LINE);
-        for (int i = 0; i < Cell.ROWS; i++) {
-            new MessageView().write(Message.VERTICAL_LINE);
-            for (int j = 0; j < Cell.COLUMNS; j++) {
-                new ColorView().write(controller.getColor(new Cell(i, j)));
-                new MessageView().write(Message.VERTICAL_LINE);
-            }
-            Console.getInstance().writeln();
-        }
-        new MessageView().writeln(Message.HORIZONTAL_LINE);
-    }
 
     protected List<Color> colors;
 
