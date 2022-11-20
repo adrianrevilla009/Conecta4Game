e7abdc5ed03a426f753496d166c6468cfadaf3ff
adrianrevilla009 <adrianrg1996@gmail.com>
2022-11-20 12:34:10 +0100

    initial point

---
+++ src/main/java/org/example/testDrivenDevelopment/intervals/ClosedMin.java
@@ -0,0 +1,19 @@
+package org.example.testDrivenDevelopment.intervals;
+
+public class ClosedMin extends Min {
+
+  public ClosedMin(double value) {
+    super(value);
+  }
+
+  @Override
+  public boolean isWithin(double value) {
+    return super.isWithin(value) || this.value == value;
+  }
+
+  @Override
+	public String toString() {
+		return "[" + this.value;
+  }	
+  
+}
---
+++ src/test/java/testDrivenDevelopment/intervals/ClosedMaxTest.java
@@ -0,0 +1,21 @@
+package testDrivenDevelopment.intervals;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.example.testDrivenDevelopment.intervals.*;
+import org.junit.jupiter.api.Test;
+
+public class ClosedMaxTest extends MaxTest {
+
+  @Override
+  protected Max createMax() {
+    return new ClosedMax(this.point.getEquals());
+  }
+
+  @Test
+  @Override
+  public void givenMaxWhenIsWithinWithEqualsValue(){
+    assertTrue(this.max.isWithin(this.point.getEquals()));
+  }
+  
+}
